aa16c93979e4bc2051403530d56f1a1972fd12d2
chidi-godwin <chidieberen1998@gmail.com>
2023-04-17 13:15:36 +0100

    create merchant resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { PrismaModule } from './prisma/prisma.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { MerchantModule } from './merchant/merchant.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TransactionModule } from './transaction/transaction.module';
       cache: true,
     }),
     TransactionModule,
+    MerchantModule,
   ],
   controllers: [AppController],
   providers: [AppService],
