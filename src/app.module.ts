532b71a27b35dc53ef7a2623e8b5044e35ab8a8e
chidi-godwin <chidieberen1998@gmail.com>
2023-04-17 12:36:22 +0100

    create prisma module to enable repository pattern

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
