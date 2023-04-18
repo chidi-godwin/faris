87a9c9d980c566a4cde8e3a61475ba96459a626c
chidi-godwin <chidieberen1998@gmail.com>
2023-04-18 07:07:28 +0100

    enable swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // configure swagger api documentation
+  const config = new DocumentBuilder()
+    .setTitle('Faris Transactions 1.0')
+    .setDescription('Faris Transactios API Documentation')
+    .setVersion('1.0')
+    .addTag('Transactions')
+    .addTag('Merchants')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
