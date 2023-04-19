3270ad10cb44d649df99e498b2578dbd8455a44c
chidi-godwin <chidieberen1998@gmail.com>
2023-04-19 16:01:14 +0100

    change welcome message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Faris Transactions API Version 1.0';
   }
 }
